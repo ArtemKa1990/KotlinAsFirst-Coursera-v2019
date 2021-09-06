6ebbe0d81b6ba489c3f1aa3a397295dee53289a7
Artem <artem_y@mail.ru>
2021-09-06 09:33:16 +0300

    Finished parse.kt: dateStrToDigit, dateDigitToStr, bestHighJump, plusMinus

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -43,9 +43,9 @@ fun timeSecondsToStr(seconds: Int): String {
  * Пример: консольный ввод
  */
 fun main() {
-    println("Введите время в формате ЧЧ:ММ:СС")
-    val line = readLine()
-    if (line != null) {
+    println("Введите дату в формате ДД МЕСЯЦ ГГГГ")
+    val line = "0 - 1"//readLine()
+    /*if (line != null) {
         val seconds = timeStrToSeconds(line)
         if (seconds == -1) {
             println("Введённая строка $line не соответствует формату ЧЧ:ММ:СС")
@@ -54,9 +54,10 @@ fun main() {
         }
     } else {
         println("Достигнут <конец файла> в процессе чтения строки. Программа прервана")
-    }
-}
+    }*/
 
+    println("Result: ${plusMinus(line ?: "")}")
+}
 
 /**
  * Средняя
@@ -69,7 +70,56 @@ fun main() {
  * Обратите внимание: некорректная с точки зрения календаря дата (например, 30.02.2009) считается неверными
  * входными данными.
  */
-fun dateStrToDigit(str: String): String = TODO()
+fun dateStrToDigit(str: String): String {
+    var day: Int = 0
+    var month: String = ""
+    var monthMaxDay = 0
+    var year: Int = 0
+    val pattern = """\d{1,2}\s[А-Яа-я]+\s\d{4}""".toRegex()
+
+    val monthVal = mapOf<String, Pair<String, Int>>(
+        "января" to Pair("01", 31),
+        "февраля" to Pair("02", 28),
+        "марта" to Pair("03", 31),
+        "апреля" to Pair("04", 30),
+        "мая" to Pair("05", 31),
+        "июня" to Pair("06", 30),
+        "июля" to Pair("07", 31),
+        "августа" to Pair("08", 31),
+        "сентября" to Pair("09", 30),
+        "октября" to Pair("10", 31),
+        "ноября" to Pair("11", 30),
+        "декабря" to Pair("12", 31)
+    )
+
+    if (pattern.matches(str)) {
+        for (wordNum in str.split(" ").indices) {
+
+            when (wordNum) {
+                0 -> day = str.split(" ")[wordNum].toInt()
+                1 -> {
+                    month = (monthVal[str.split(" ")[wordNum]]?.first ?: "")
+                    monthMaxDay = (monthVal[str.split(" ")[wordNum]]?.second ?: 0)
+                }
+                2 -> year = str.split(" ")[wordNum].toInt()
+            }
+            //println("Day $day; month $month; year $year; str: ${str.split(" ")[wordNum]}")
+            if (day > 31 || day < 1 || (wordNum == 1 && month == "")) {
+                return ""
+            }
+            // Високосный год
+            if ((year % 4 == 0 && month == "02" && day > monthMaxDay + 1)) {
+                return ""
+            } else if (year % 4 != 0 && day > monthMaxDay) {
+                return ""
+            }
+        }
+    }
+    if (day == 0 || month == "" || year == 0) {
+        return ""
+    }
+    return String.format("%02d.%s.%4d", day, month, year)
+}
 
 /**
  * Средняя
@@ -81,7 +131,58 @@ fun dateStrToDigit(str: String): String = TODO()
  * Обратите внимание: некорректная с точки зрения календаря дата (например, 30 февраля 2009) считается неверными
  * входными данными.
  */
-fun dateDigitToStr(digital: String): String = TODO()
+fun dateDigitToStr(digital: String): String {
+    val monthVal = mapOf<String, Pair<String, Int>>(
+        "01" to Pair("января", 31),
+        "02" to Pair("февраля", 28),
+        "03" to Pair("марта", 31),
+        "04" to Pair("апреля", 30),
+        "05" to Pair("мая", 31),
+        "06" to Pair("июня", 30),
+        "07" to Pair("июля", 31),
+        "08" to Pair("августа", 31),
+        "09" to Pair("сентября", 30),
+        "10" to Pair("октября", 31),
+        "11" to Pair("ноября", 30),
+        "12" to Pair("Декабря", 31)
+    )
+
+    var day: Int = 0
+    var month: String = ""
+    var monthMaxDay = 0
+    var year: Int = 0
+    var buf: String = ""
+    val pattern = """\d{1,2}.\d{1,2}.\d{4}""".toRegex()
+
+    if (pattern.matches(digital)) {
+        for (wordNum in digital.split(".").indices) {
+            buf = digital.split(".")[wordNum]
+            when (wordNum) {
+                0 -> day = buf.toInt()
+                1 -> {
+                    month = (monthVal[digital.split(".")[wordNum]]?.first ?: "")
+                    monthMaxDay = (monthVal[digital.split(".")[wordNum]]?.second ?: 0)
+                }
+                2 -> year = buf.toInt()
+            }
+            println("Day $day; month $month; year $year; str: ${digital.split(".")[wordNum]}")
+            if (day > 31 || day < 1 || (wordNum == 1 && month == "")) {
+                return ""
+            }
+            // Високосный год
+            if ((year % 4 == 0 && month == "02" && day > monthMaxDay + 1)) {
+                return ""
+            } else if (year % 4 != 0 && day > monthMaxDay) {
+                return ""
+            }
+        }
+    }
+
+    if (day == 0 || month == "" || year == 0) {
+        return ""
+    }
+    return String.format("%d %s %4d", day, month, year)
+}
 
 /**
  * Средняя
@@ -122,7 +223,39 @@ fun bestLongJump(jumps: String): Int = TODO()
  * При нарушении формата входной строки, а также в случае отсутствия удачных попыток,
  * вернуть -1.
  */
-fun bestHighJump(jumps: String): Int = TODO()
+fun bestHighJump(jumps: String): Int {
+    var result = -1
+    var attemptHigh = 0
+    var attemptType = ""
+    var typeAttemptPattern = Regex("""[\\+,-/,%]""")
+    var typeExceptionsPattern = Regex("""[^\\+,-/,%]|(\d+[^\s])""")
+    //var pattern = """^(\d+\s[+,-/,%])(\s\d+\s[+,-/,%])+$""".toRegex()
+
+    //println("Match: ${pattern.matchEntire(jumps)}; pattern value: ${jumps.split(pattern)}")
+
+    for (jump in jumps.split(" ")) {
+
+        if (jump.toIntOrNull() != null && jump.toInt() > result) {
+            attemptHigh = jump.toInt()
+        } else if (jump.toIntOrNull() == null && typeAttemptPattern.matches(jump)) {
+            attemptType = jump
+        } else if (typeExceptionsPattern.containsMatchIn(jump)) {
+            result = -1
+            break
+        }
+
+        if (attemptHigh > result && attemptType == "+") {
+            result = attemptHigh
+            attemptHigh = 0
+            attemptType = ""
+        } else if (attemptHigh != 0 && attemptType != "") {
+            attemptHigh = 0
+            attemptType = ""
+        }
+    }
+
+    return result
+}
 
 /**
  * Сложная
@@ -133,7 +266,39 @@ fun bestHighJump(jumps: String): Int = TODO()
  * Вернуть значение выражения (6 для примера).
  * Про нарушении формата входной строки бросить исключение IllegalArgumentException
  */
-fun plusMinus(expression: String): Int = TODO()
+fun plusMinus(expression: String): Int {
+    var result = 0
+    var numValue = 0
+    var typeAction = ""
+    var truePattern = Regex("""(\d+)|((\d+\s[+,-]\s\d+)*((\s[+,-]\s\d+))*)""")
+    var exceptionPattern = Regex("""(\d+[^\s])|([+,-][^\s])|(\d+\s\d)|([+,-]\s[+,-])""")
+
+    if (truePattern.matchEntire(expression) == null) {
+        throw IllegalArgumentException("Wrong row")
+    }
+    for (value in expression.split(" ")) {
+        if (value.toIntOrNull() != null) {
+            numValue = value.toInt()
+        } else {
+            typeAction = value
+        }
+        println("numValue: $numValue typeAction: $typeAction")
+        if (result == 0 && numValue > 0 && typeAction == "") {
+            result = numValue
+            numValue = 0
+        } else if (numValue != 0 && typeAction != "") {
+            println("result Before Action: $result")
+            when (typeAction) {
+                "+" -> result += numValue
+                "-" -> result -= numValue
+            }
+            println("result After Action: $result")
+            numValue = 0
+            typeAction = ""
+        }
+    }
+    return result
+}
 
 /**
  * Сложная
